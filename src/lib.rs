8f9fe64021936b4397413b6a6c137d3a8c00f551
Eduardo Flores <edfloreshz@gmail.com>
2025-01-06 01:53:43 +0100

    add: features for winebridge and server

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,9 @@ pub mod proto {
     tonic::include_proto!("winebridge");
     tonic::include_proto!("bottles");
 }
+
+#[cfg(feature = "winebridge")]
+pub mod winebridge;
+
+#[cfg(feature = "server")]
+pub mod server;
