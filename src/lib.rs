21015d909e758cde02d0fcaf319f5395f6103844
Eduardo Flores <edfloreshz@proton.me>
2025-01-15 18:40:17 +0100

    update: workspace

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,3 @@ pub mod proto {
     tonic::include_proto!("winebridge");
     tonic::include_proto!("bottles");
 }
-
-#[cfg(feature = "winebridge")]
-pub mod winebridge;
-
-#[cfg(feature = "server")]
-pub mod server;
