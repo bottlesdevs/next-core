e87847a70d9c52d079d53eb5a528154dec153274
Inam Ul Haq <inam.ul.haq.s.shah@oracle.com>
2026-05-11 10:51:45 +0530

    error: Add result alias and logging helpers

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,7 @@
 use thiserror::Error;
 
+pub type Result<T> = std::result::Result<T, Error>;
+
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("I/O: {0}")]
@@ -10,4 +12,30 @@ pub enum Error {
     Transport(#[from] tonic::transport::Error),
     #[error("gRPC status: {0}")]
     Status(#[from] tonic::Status),
+}
+
+#[allow(dead_code)]
+pub trait ResultExt<T, E> {
+    fn log_error(self) -> Option<T>;
+    fn log_warn(self) -> Option<T>;
+    fn log_info(self) -> Option<T>;
+    fn log_debug(self) -> Option<T>;
+}
+
+impl<T, E: std::error::Error> ResultExt<T, E> for std::result::Result<T, E> {
+    fn log_error(self) -> Option<T> {
+        self.inspect_err(|e| tracing::error!("{e}")).ok()
+    }
+
+    fn log_warn(self) -> Option<T> {
+        self.inspect_err(|e| tracing::warn!("{e}")).ok()
+    }
+
+    fn log_info(self) -> Option<T> {
+        self.inspect_err(|e| tracing::info!("{e}")).ok()
+    }
+
+    fn log_debug(self) -> Option<T> {
+        self.inspect_err(|e| tracing::debug!("{e}")).ok()
+    }
 }
