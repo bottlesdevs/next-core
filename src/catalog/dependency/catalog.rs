5c23ddcf86d19366693ddd2a0831c59a60acb99c
Inam Ul Haq <inam123451@gmail.com>
2026-06-26 11:47:38 +0530

    catalog: Implement Catalog trait

--- src/catalog/dependency/catalog.rs
+++ src/catalog/dependency/catalog.rs
@@ -1,19 +1,19 @@
 use super::Dependency;
+use crate::catalog::Catalog;
 use serde::{Deserialize, Serialize};
+use std::num::NonZeroU32;
+
+static CATALOG_VERSION: NonZeroU32 = NonZeroU32::new(1).unwrap();
 
 #[derive(Debug, Clone, Deserialize, Eq, PartialEq, Serialize)]
-pub struct Catalog {
-    schema_version: u32,
+pub struct DependencyCatalog {
+    schema_version: NonZeroU32,
     components: Vec<Dependency>,
 }
 
-impl Catalog {
-    pub fn iter(&self) -> impl ExactSizeIterator<Item = &Dependency> + DoubleEndedIterator {
-        self.components.iter()
-    }
-}
+pub struct DependencyCatalogQuery {}
 
-impl IntoIterator for Catalog {
+impl IntoIterator for DependencyCatalog {
     type IntoIter = std::vec::IntoIter<Dependency>;
     type Item = Dependency;
 
@@ -22,11 +22,28 @@ impl IntoIterator for Catalog {
     }
 }
 
-impl<'catalog> IntoIterator for &'catalog Catalog {
+impl<'catalog> IntoIterator for &'catalog DependencyCatalog {
     type IntoIter = std::slice::Iter<'catalog, Dependency>;
     type Item = &'catalog Dependency;
 
     fn into_iter(self) -> Self::IntoIter {
         self.components.iter()
+    }
+}
+
+impl Catalog for DependencyCatalog {
+    type Item = Dependency;
+    type Query = DependencyCatalogQuery;
+
+    fn version(&self) -> std::num::NonZeroU32 {
+        CATALOG_VERSION
+    }
+
+    fn iter(&self) -> impl ExactSizeIterator<Item = &Self::Item> + DoubleEndedIterator {
+        self.components.iter()
+    }
+
+    fn query(&self) -> Self::Query {
+        todo!()
     }
 }
