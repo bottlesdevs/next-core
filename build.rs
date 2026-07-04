28e3986bfbcd035e9b61ad417a48fe0c6bba3d73
Inam Ul Haq <inam123451@gmail.com>
2026-07-05 02:07:54 +0530

    build: rerun proto generation when proto files change

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    println!("cargo:rerun-if-changed=proto/bottles.proto");
+    println!("cargo:rerun-if-changed=proto/winebridge.proto");
+
     tonic_prost_build::configure().compile_protos(
         &["proto/bottles.proto", "proto/winebridge.proto"],
         &["proto/"],
