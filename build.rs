e7f73a43850aee1ed9c9897c897141a508286b9a
Inam Ul Haq <inam123451@gmail.com>
2026-07-14 21:02:29 +0530

    virgo: Use fvs crate

--- build.rs
+++ build.rs
@@ -3,14 +3,9 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     println!("cargo:rerun-if-changed=proto/bottles.proto");
     println!("cargo:rerun-if-changed=proto/winebridge.proto");
-    println!("cargo:rerun-if-changed=proto/fvs2d.proto");
 
     tonic_prost_build::configure().compile_protos(
-        &[
-            "proto/bottles.proto",
-            "proto/winebridge.proto",
-            "proto/fvs2d.proto",
-        ],
+        &["proto/bottles.proto", "proto/winebridge.proto"],
         &["proto/"],
     )?;
     Ok(())
--- src/lib.rs
+++ src/lib.rs
@@ -10,13 +10,8 @@ mod winebridge;
 pub mod proto {
     tonic::include_proto!("winebridge");
     tonic::include_proto!("bottles");
-
-    pub mod fvs2d {
-        tonic::include_proto!("fvs2d.v1");
-    }
 }
 
-pub use crate::virgo::{Layer, LayerRevision, VirgoDaemon};
 pub use crate::winebridge::{
     DllOverride, DllOverrideMode, Drive, LaunchRequest, PathInfo, PathKind, Process, RegistryHive,
     RegistryKey, RegistryKeyValue, RegistryMultiString, RegistryValue, Service, ServiceStartType,
