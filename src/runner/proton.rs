9da0915a0e964db572a31a980e947ebdf1b8db49
Inam Ul Haq <inam123451@gmail.com>
2026-07-17 21:41:57 +0530

    runner: implement Proton through UMU

--- src/runner/proton.rs
+++ src/runner/proton.rs
@@ -1,10 +1,10 @@
 use std::{
     io::{self, ErrorKind},
     path::{Path, PathBuf},
-    process::Command,
+    process::{Child, Command},
 };
 
-use super::{PrefixConfig, Runner, RunnerCommand, RunnerError, Wine};
+use super::{Runner, RunnerCommand, RunnerError};
 use crate::error::Result;
 
 /// Proton runner implementation
@@ -13,84 +13,95 @@ use crate::error::Result;
 /// numerous patches and enhancements over standard Wine, making it particularly
 /// effective for running Windows games through Steam or standalone.
 ///
-/// # Note
-/// When used outside of Steam, Proton requires specific environment variables:
-/// - `STEAM_COMPAT_DATA_PATH`: Path to store compatibility data
-/// - `STEAM_COMPAT_CLIENT_INSTALL_PATH`: Steam installation directory
 #[derive(Debug)]
 pub struct Proton {
-    _wine: Wine,
-    executable: PathBuf,
+    proton_path: PathBuf,
+    umu_executable: PathBuf,
 }
 
 impl Proton {
-    pub fn new(executable: impl AsRef<Path>) -> Result<Self> {
-        if !executable.as_ref().exists() {
-            return Err(io::Error::new(ErrorKind::NotFound, "Proton executable not found").into());
+    pub fn new(proton_path: impl AsRef<Path>, umu_executable: impl AsRef<Path>) -> Result<Self> {
+        if !proton_path.as_ref().join("proton").is_file() {
+            return Err(io::Error::new(ErrorKind::NotFound, "Proton runner not found").into());
         }
 
-        if !executable.as_ref().is_file() {
-            return Err(
-                io::Error::new(ErrorKind::InvalidInput, "Proton executable is not a file").into(),
-            );
+        if !umu_executable.as_ref().is_file() {
+            return Err(io::Error::new(ErrorKind::NotFound, "UMU executable not found").into());
         }
-
-        let parent_directory = executable
-            .as_ref()
-            .parent()
-            .expect("Executable should have a parent directory");
-        let wine = Wine::new(parent_directory.join("files/bin/wine"))?;
 
         Ok(Self {
-            _wine: wine,
-            executable: executable.as_ref().to_path_buf(),
+            proton_path: proton_path.as_ref().to_path_buf(),
+            umu_executable: umu_executable.as_ref().to_path_buf(),
         })
     }
 }
 
 impl Runner for Proton {
-    fn run(
-        &self,
-        prefix: &PrefixConfig,
-        command: RunnerCommand,
-    ) -> crate::error::Result<std::process::Child> {
-        if !prefix.is_proton() {
-            return Err(RunnerError::ProtonEnvVarsMissing.into());
-        }
-
-        Command::new(&self.executable)
-            .arg("run")
+    fn run(&self, prefix: &Path, command: RunnerCommand) -> Result<Child> {
+        Command::new(&self.umu_executable)
             .arg(command.executable)
             .args(command.args)
-            .envs(prefix.to_env())
+            .env("WINEPREFIX", prefix)
+            .env("WINEARCH", "win64")
+            .env("PROTONPATH", &self.proton_path)
             .envs(command.envs)
             .spawn()
             .map_err(Into::into)
     }
 
-    fn initialize_prefix(&self, prefix: &PrefixConfig) -> Result<()> {
-        if !prefix.is_proton() {
-            return Err(RunnerError::ProtonEnvVarsMissing.into());
-        }
-
-        // Safe to unwrap here since we already verified this is a proton prefix
-        let compat_data_path = prefix.compat_data_path.as_deref().unwrap();
-        if !compat_data_path.exists() {
-            // Proton will fail to run compat_data_path doesn't exist, so create it
-            std::fs::create_dir_all(compat_data_path)?;
-        }
-
-        let command = RunnerCommand::builder()
-            .executable("wineboot")
-            .arg("--init")
-            .build()
-            .map_err(Into::<RunnerError>::into)?;
-
+    fn initialize_prefix(&self, prefix: &Path) -> Result<()> {
+        let command = RunnerCommand::new("wineboot").arg("--init");
         let status = self.run(prefix, command)?.wait()?;
         if !status.success() {
             return Err(RunnerError::PrefixInitFailed.into());
         }
-
         Ok(())
+    }
+}
+
+#[cfg(all(test, unix))]
+mod tests {
+    use std::{fs, os::unix::fs::PermissionsExt};
+
+    use super::*;
+
+    #[test]
+    fn all_proton_operations_run_through_umu_with_prefix_environment() {
+        let root = std::env::temp_dir().join(uuid::Uuid::new_v4().to_string());
+        let proton_path = root.join("proton");
+        let umu = root.join("umu-run");
+        let log = root.join("umu.log");
+        fs::create_dir_all(&proton_path).unwrap();
+        fs::write(proton_path.join("proton"), []).unwrap();
+        fs::write(
+            &umu,
+            format!(
+                "#!/bin/sh\nlog='{}'\nprintf '%s|%s|%s|' \"$PROTONPATH\" \"$WINEPREFIX\" \"$WINEARCH\" >> \"$log\"\nprintf '<%s>' \"$@\" >> \"$log\"\nprintf '\\n' >> \"$log\"\n",
+                log.display()
+            ),
+        )
+        .unwrap();
+        fs::set_permissions(&umu, fs::Permissions::from_mode(0o755)).unwrap();
+
+        let runner = Proton::new(&proton_path, &umu).unwrap();
+        let prefix = root.join("prefix");
+        runner.initialize_prefix(&prefix).unwrap();
+        runner
+            .run(&prefix, RunnerCommand::new("game.exe").arg("--flag"))
+            .unwrap()
+            .wait()
+            .unwrap();
+
+        let environment = format!("{}|{}|win64|", proton_path.display(), prefix.display());
+        assert_eq!(
+            fs::read_to_string(&log).unwrap(),
+            [
+                format!("{environment}<wineboot><--init>\n"),
+                format!("{environment}<game.exe><--flag>\n"),
+            ]
+            .concat()
+        );
+
+        fs::remove_dir_all(root).unwrap();
     }
 }
