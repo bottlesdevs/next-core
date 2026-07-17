d984e991bdcfeac4d10c954610518aed8e343875
Inam Ul Haq <inam123451@gmail.com>
2026-07-17 21:42:24 +0530

    bottle: define bottle errors and exports

---
+++ src/bottle/mod.rs
@@ -0,0 +1,12 @@
+#[allow(clippy::module_inception)]
+mod bottle;
+mod error;
+mod manager;
+mod virgo;
+
+#[cfg(test)]
+mod tests;
+
+pub use bottle::{Bottle, BottleType, Program};
+pub use error::BottleError;
+pub use manager::{BottleManager, BottleManagerConfig};
